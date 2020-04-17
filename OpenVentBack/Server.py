c60f938260fd38debde2aec645feb0b9406b1132
Unknown <fabijoey@kabelmail.de>
2020-04-17 15:36:04 +0200

    Added Threading

--- OpenVentBack/Server.py
+++ OpenVentBack/Server.py
@@ -1,22 +1,57 @@
 import socket
+import threading
+import time
+
+
+class UpdateThread(threading.Thread):
+    def __init__(self, name='UpdateThread'):
+        self._stopevent = threading.Event()
+        self._sleepperiod = 1.0
+        self._recievers = []
+
+        threading.Thread.__init__(self, name=name)
+
+    def run(self):
+        print("%s starts" % (self.getName(),))
+
+        while not self._stopevent.is_set():
+            for c in self._recievers:
+                try:
+                    c.send(b'Hello')
+                except socket.error:
+                    self._recievers.remove(c)
+            self._stopevent.wait(self._sleepperiod)
+
+        print("%s ends" % (self.getName(),))
+
+    def join(self, timeout=None):
+        self._stopevent.set()
+        threading.Thread.join(self, timeout)
+
+    def add(self,_conn=None):
+        if _conn is not None:
+            self._recievers.append(_conn)
 
 
 if __name__ == '__main__':
-    TCP_IP = '127.0.0.1'
+    NUMBER_OF_DEVICES = 5
     TCP_PORT = 5005
     BUFFER_SIZE = 1024
 
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.bind((socket.gethostname(), TCP_PORT))
-    s.listen(5)
+    t = UpdateThread()
 
+    t.start()
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind(('127.0.0.1', TCP_PORT))
+    s.listen(NUMBER_OF_DEVICES)
 
     while True:
         conn, addr = s.accept()
         print('Connection address:', addr)
-        data = conn.recv(BUFFER_SIZE)
-        if not data: break
-        print("received data:", data)
-        conn.send(data)
+        t.add(conn)
     conn.close()
+    t.join()
+
+
 
