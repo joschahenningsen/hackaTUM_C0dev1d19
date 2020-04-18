81a38212ca50e4d4ccb940a5773f7af299cd78d8
HalloIBimsDerFabian <fabian.proebstle@tum.de>
2020-04-18 16:53:26 +0200

    new patch

--- OpenVentBack/Threads.py
+++ OpenVentBack/Threads.py
@@ -162,7 +162,7 @@ class AlarmListener(threading.Thread):
                         temp=value['processed']['triggerSettings']
                         print(msg[1])
                         cursor = conndb.cursor()
-                        cursor.execute("INSERT INTO screenshots (fio2,ie,mve,peep,rr,vt, humidity, pressure_max, id,vent) VALUES(%d,%d, %d, %d, %d, %d, %d, %d, %s,%s)" % (temp['FiO2'],temp['IE'],temp['MVe'],temp['PEEP'],temp['RR'],temp['VT'], temp['humidity'], temp['pressure_max'],msg[1],key))
+                        cursor.execute("INSERT INTO screenshots (fio2,ie,mve,peep,rr,vt, humidity, pressure_max, id,vent) VALUES(%d,%d, %d, %d, %d, %d, %d, %d, $token$%s$token$,$token$%s$token$)" % (temp['FiO2'],temp['IE'],temp['MVe'],temp['PEEP'],temp['RR'],temp['VT'], temp['humidity'], temp['pressure_max'],msg[1],key))
                         conndb.commit()
                         cursor.close()
                 elif msg[0] == "resume":
