017b3076f6309f2232b930afb95088a3c5deba69
HalloIBimsDerFabian <fabian.proebstle@tum.de>
2020-04-18 18:20:54 +0200

    sdsd

--- OpenVentBack/Threads.py
+++ OpenVentBack/Threads.py
@@ -176,8 +176,9 @@ class AlarmListener(threading.Thread):
                 rows = cursor.fetchall()
                 print("test3")
                 dict2 = {}
-                print(rows)
+
                 for row in rows:
+                    print(row)
                     dict2[row[9]]={'FiO2':row[0], 'IE':row[1],'MVe':row[2],'PEEP':row[4],'RR':row[5],'VT':row[6],'humidity':row[7],'pressure_max':row[8]}
                 print("testfinal")
                 print(("%s\n" % json.dumps(dict2)))
