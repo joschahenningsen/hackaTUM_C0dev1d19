6c4e8e00ecd247bc3779dc0b880b776404a955ef
HalloIBimsDerFabian <fabian.proebstle@tum.de>
2020-04-18 18:32:36 +0200

    Complete Database

--- OpenVentBack/Threads.py
+++ OpenVentBack/Threads.py
@@ -176,12 +176,10 @@ class AlarmListener(threading.Thread):
                 rows = cursor.fetchall()
                 print("test3")
                 dict2 = {}
-
+                req = data
                 for row in rows:
-                    print(row)
-                    dict2[row[8]]={'FiO2':row[0], 'IE':row[1],'MVe':row[2],'PEEP':row[3],'RR':row[4],'VT':row[5],'humidity':row[6],'pressure_max':row[7]}
-                print("testfinal")
-                print(("%s\n" % json.dumps(dict2)))
+                    temp=req[row[8]]['processed']['triggerSettings']
+                    dict2[row[8]]={'FiO2_Old':row[0],'FiO2':temp['FiO2'], 'IE_Old':row[1],'IE':temp['IE'],'MVe_Old':row[2],'MVe':temp['MVe'],'PEEP_Old':row[3],'PEEP':temp['PEEP'],'RR_Old':row[4],'RR':temp['RR'],'VT_Old':row[5],'VT':temp['VT'],'humidity_Old':row[6],'humidity':temp['humidity'],'pressure_max_Old':row[7],'pressure_max':temp['pressure_max']}
                 self._conn.send(("%s\n" % json.dumps(dict2)).encode())
                 cursor.execute("DELETE from screenshots where id=$token$%s$token$" % (msg[1]))
                 conndb.commit()
