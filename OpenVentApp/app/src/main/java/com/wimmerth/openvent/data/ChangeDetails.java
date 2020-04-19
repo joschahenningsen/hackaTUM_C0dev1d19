3a7d0cb73e365b1b0cca55f5770bf0eb2564f241
wimmerth <ttt.wimmer@gmail.com>
2020-04-19 11:08:19 +0200

    hopefully works now

--- OpenVentApp/app/src/main/java/com/wimmerth/openvent/data/ChangeDetails.java
+++ OpenVentApp/app/src/main/java/com/wimmerth/openvent/data/ChangeDetails.java
@@ -12,22 +12,22 @@ class ChangeDetails {
     List<Change> getChanges() {
         ArrayList<Change> ret = new ArrayList<>();
         if (Float.compare(FiO2_Old, Float.parseFloat(FiO2)) != 0) {
-            ret.add(new Change(device_id, "FiO₂", FiO2_Old + "%", FiO2 + "%"));
+            ret.add(new Change(device_id, "FiO₂", FiO2_Old + "%", Float.parseFloat(FiO2) + "%"));
         }
         if (Float.compare(IE_Old, IE) != 0) {
             ret.add(new Change(device_id, "IE", IE_Old + "", IE + ""));
         }
         if (Float.compare(MVe_Old, Float.parseFloat(MVe)) != 0) {
-            ret.add(new Change(device_id, "MVe", MVe_Old + " mL", MVe + " mL"));
+            ret.add(new Change(device_id, "MVe", MVe_Old + " mL", Float.parseFloat(MVe) + " mL"));
         }
         if (Float.compare(PEEP_Old, PEEP) != 0) {
             ret.add(new Change(device_id, "PEEP", PEEP_Old + " cmH₂O", PEEP + " cmH₂O"));
         }
         if (Float.compare(humidity_Old, Float.parseFloat(humidity)) != 0) {
-            ret.add(new Change(device_id, "humidity", humidity_Old + "%", humidity + "%"));
+            ret.add(new Change(device_id, "humidity", humidity_Old + "%", Float.parseFloat(humidity) + "%"));
         }
         if (Integer.parseInt(pressure_max) != pressure_max_Old) {
-            ret.add(new Change(device_id, "Pmax", pressure_max_Old + " cmH₂O", pressure_max + " cmH₂O"));
+            ret.add(new Change(device_id, "Pmax", pressure_max_Old + " cmH₂O", Integer.parseInt(pressure_max) + " cmH₂O"));
         }
         if (VT_Old != VT) {
             ret.add(new Change(device_id, "VT", VT_Old + " mL", VT + " mL"));
