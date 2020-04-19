f16eb758c89aeb7de50fe8c348083550cbfca736
HalloIBimsDerFabian <fabian.proebstle@tum.de>
2020-04-19 08:48:15 +0200

    der broken api fix

--- OpenVentBack/Threads.py
+++ OpenVentBack/Threads.py
@@ -166,7 +166,7 @@ class AlarmListener(threading.Thread):
                     temp=value['processed']['triggerSettings']
                     cursor = conndb.cursor()
                     print(temp['IE'])
-                    cursor.execute("INSERT INTO screenshots (fio2,ie,mve,peep,rr,vt, humidity, pressure_max, id,vent) VALUES(%s,%e, %s, %e, %d, %d, %s, %s, $token$%s$token$,$token$%s$token$)" % (temp['FiO2'],temp['IE'],temp['MVe'],temp['PEEP'],temp['RR'],temp['VT'], temp['humidity'], temp['pressure_max'],msg[1],key))
+                    cursor.execute("INSERT INTO screenshots (fio2,ie,mve,peep,rr,vt, humidity, pressure_max, id,vent) VALUES(%s,%s, %s, %s, %s, %s, %s, %s, $token$%s$token$,$token$%s$token$)" % (temp['FiO2'],temp['IE'],temp['MVe'],temp['PEEP'],temp['RR'],temp['VT'], temp['humidity'], temp['pressure_max'],msg[1],key))
                     conndb.commit()
                 cursor.close()
             elif msg[0] == "resume":
